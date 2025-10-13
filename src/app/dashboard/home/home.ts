53701ea11e48989b44d01c3769db1edfb44310b0
Vinay Tiwari <xvinaytiwari@gmail.com>
2025-10-13 14:26:42 +0530

    inital

---
+++ src/app/dashboard/home/home.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+// import { RouterOutlet } from "@angular/router/router_module.d";
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-home',
+  imports: [RouterLink],
+  templateUrl: './home.html',
+  styleUrl: './home.css'
+})
+export class Home {
+
+}
